f07b5860d4473a694cf8bbf3c151a5a4d82d0010
Daouda-Soumare14 <115658162+Daouda-Soumare14@users.noreply.github.com>
2023-10-19 03:26:48 +0200

    Add files via upload

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    int ageUtilisateur(10);
+    string nomUtilisateur("DAOUDA SOUMARE");
+    
+    cout << "Vous vous appelez " << nomUtilisateur << " et vous avez : " << ageUtilisateur << " ans " << endl;
+    return 0;
+}
---
+++ variable.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int ageUtilisateur(16);
+
+    int nombreAmis(432);      //Le nombre d'amis de l'utilisateur
+
+    double pi(3.14159);
+
+    bool estMonAmi(true);    //Cet utilisateur est-il mon ami ?
+
+    char lettre('a');
+
+    return 0;
+}
